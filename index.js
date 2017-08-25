39d9ded8abee3b0f0c57d02330398a82ffce28da
Anthony Nixon <ajn0592@gmail.com>
2017-08-24 23:59:42 -0500

    add simple data pulling

--- index.js
+++ index.js
@@ -12,4 +12,14 @@ angular.module("Magic2Nite", ['ngRoute'])
 
     .controller("homeCtrl", function($scope, $http, $location) {
         $scope.path = $location.path();
+    })
+
+    .controller("podCtrl", function($scope, $http, $location, $routeParams) {
+        $scope.backendHost = "http://localhost:3000";
+        $scope.podCode = $routeParams.shortCode;
+        $scope.pod = {};
+
+        $http.get($scope.backendHost + "/pod/" + $scope.podCode).then(function (response) {
+            $scope.pod = response.data;
+        });
     });
